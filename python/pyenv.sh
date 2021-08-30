8a91a4717631a70b271ccd46856bf32b3c8ae0ab
Yamaguchi Teppei <43245087+yamaguchi-milkcocholate@users.noreply.github.com>
2021-08-30 15:39:36 +0900

    [update] pyenv

---
+++ python/pyenv.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zprofile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
+echo 'eval "$(pyenv init -)"' >> ~/.zprofile
+source ~/.zprofile
